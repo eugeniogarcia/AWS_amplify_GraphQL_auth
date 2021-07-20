2eec07b15c2df5c2423b2c119927066b81074f2b
Eugenio Garcia San Martin <egsmartin@gmail.com>
2021-07-20 08:57:56 +0200

    Sin errores de compilacion

--- README.md
+++ README.md
@@ -27,8 +27,8 @@ Especificamos los siguientes valores:
 |Cuenta|Valor|
 |-----|-----|
 |euge|egsmartin@hotmail.com|
-|Client ID|AKIAXBEVMWBDPGUDK6ZQ|
-|Client Secret|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|
+|Client ID|AKIAXBEVMWBDJKJG6AM4|
+|Client Secret|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|
 
 Instalamos las dependencias de Amplifly:
 
@@ -42,123 +42,71 @@ Lo inicializamos:
 amplify init
 ```
 
-## Crear un Backend (Autenticación)
-
-Añadimos el backend de autenticación:
-
-```ps
-amplify add auth
-
-amplify push
-```
-
-## Crear un Backend (Lambda)
-
-```ps
-amplify add function
-```
-
-## Eliminar un Backend
-
-```ps
-amplify remove auth
-```
-
-## Eliminar un proyecto
-
-```ps
-amplify delete
-```
-
 # Codificación
 
-En _index_ añadimos Amplify:
-
-```js
-//Configura React para trabajar con Amplify
-import Amplify from 'aws-amplify'
-import config from './aws-exports'
-Amplify.configure(config)
-```
-
-## Autenticación
-
-Incluimos la App en un wrapper:
-
-```js
-export default withAuthenticator(App)
-```
-
-Incluimos en la respuesta un control para salir
-
-```js
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <h1>Hola desde AWS Amplify</h1>
-      <AmplifySignOut />
-    </div>
-```
-
-## Función
-
-Para crear una función haremos lo siguiente:
-
 ```ps
-amplify add function
+npm install antd uuid
 ```
-
-Esto crear un directorio en nuestro projecto en la carpeta _backend_. Cada función tiene una _app.js_ donde se implementan los end-points de nuestra función, usando Express - porque es el runtime que hemos elegido al crear la fución.
-
-## Api
-
-Para crear una api hacemos:
 
 ```ps
 amplify add api
+
+
+C:\Users\Eugenio\Downloads\app-gql>amplify add api
+? Please select from one of the below mentioned services: GraphQL
+? Provide API name: notesapi
+? Choose the default authorization type for the API API key
+? Enter a description for the API key: Mi clave de acceso
+? After how many days from now the API key should expire (1-365): 365
+? Do you want to configure advanced settings for the GraphQL API No, I am done.
+? Do you have an annotated GraphQL schema? No
+? Choose a schema template: One-to-many relationship (e.g., “Blogs” with “Posts” and “Comments”)
 ```
 
-Para publicar los cambios:
+Elegimos GraphQL como tipo de API. En _amplify\backend\api\notesapi_ se genera el código. En _schema.graphql_ tenemos la definición del esquema:
+
+```js
+type Note @model {
+  id: ID!
+  clientId: ID
+  name: String!
+  description: String
+  completed: Boolean
+}
+```
+The GraphQL Transform library allows you to annotate a GraphQL schema with different directives like @model, @connection, @auth, and others. The @model directive we used in this schema will transform the base Note type into an expanded AWS AppSync GraphQL API complete with:
+
+- Additional schema definitions for queries and mutations (Create, Read, Update, Delete, and List operations)
+- Additional schema definitions for GraphQL subscriptions
+- DynamoDB database
+- Resolver code for all GraphQL operations mapped to DynamoDB database
+
+Desplegamos la api:
 
 ```ps
 amplify push
 
-Current Environment: dev
+√ Generated GraphQL operations successfully and saved at src\graphql
+√ Code generated successfully and saved in file src\API.ts
+√ All resources are updated in the cloud
 
-| Category | Resource name | Operation | Provider plugin   |
-| -------- | ------------- | --------- | ----------------- |
-| Function | miapp649af301 | Create    | awscloudformation |
-| Api      | cryptoapi     | Create    | awscloudformation |
-| Auth     | miappbfd4fb76 | Delete    | awscloudformation |
+GraphQL endpoint: https://tl2do3hdpjgpjb4u2aakug5zse.appsync-api.eu-west-1.amazonaws.com/graphql
+GraphQL API KEY: da2-btotoud4ufbl5japgddkfm5udm
 ```
 
-Podemos ver el estado de Amplify:
+Podemos probar la api - REST o GraphQL - usando la consola de amplify:
 
 ```ps
-amplify status
-
-Current Environment: dev
-
-| Category | Resource name | Operation | Provider plugin   |
-| -------- | ------------- | --------- | ----------------- |
-| Function | miapp649af301 | No Change | awscloudformation |
-| Api      | cryptoapi     | No Change | awscloudformation |
-
-REST API endpoint: https://fqk9vnz7qf.execute-api.eu-west-2.amazonaws.com/dev
+amplify console api
 ```
 
-Como podemos ver el comando nos indica que tenemos _pendiente_ de subir a AWS.
+## Codigo
 
-## Axios
+Añadimos en index.ts que vamos a utilizar _Amplify_:
 
-Con Axios tenemos una librería que permite, tanto desde _Node.js_ como desde un navegador hacer peticiones http.
-
-Instalaremos axios en la aplicación js que se ha creado para nuestra función, porque es desde nuestra función desde la que queremos hacer uso de ella. Podemos encontrar la aplicación que se ha creado para esta función en _amplify\backend\function\miapp649af301_:
-
-```ps
-cd C:\Users\Eugenio\Downloads\mi-app\amplify\backend\function\miapp649af301\src
-
-npm install axios
+```js
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 ```
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,204 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useReducer } from 'react'
 import './App.css';
 
+import API, { graphqlOperation } from '@aws-amplify/api'
+import { listNotes } from './graphql/queries'
+import { updateNote as UpdateNote, createNote as CreateNote, deleteNote as DeleteNote } from './graphql/mutations'
+import { onCreateNote } from './graphql/subscriptions'
+
+import { Input, List, Button } from 'antd'
+import 'antd/dist/antd.css'
+import { uuid } from 'uuidv4';
+import CSS from 'csstype';
+
+const styles = {
+  container: { padding: '20' } as CSS.Properties,
+  input: { marginBottom: '10' } as CSS.Properties,
+  item: { textAlign: 'left' } as CSS.Properties,
+  p: { color: '#1890ff' } as CSS.Properties
+}
+
+type nota = {
+  id?: string 
+  clientId: string
+  name: string
+  description?: string
+  completed?: boolean
+}
+
+type estado={
+  notes: nota[],
+  loading: boolean,
+  error: boolean,
+  form: { name: string, description: string }
+}
+
+type accion=
+  {
+    type: 'ERROR' | 'RESET_FORM'
+  } |
+  {
+    type: 'SET_INPUT'
+    name: string,
+    value:string
+  } |
+  {
+    type: 'ADD_NOTE'
+    note: nota,
+  } |
+  {
+    type: 'SET_NOTES'
+    notes: nota[],
+  } 
+  
+const initialState: estado = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
+}
+
+const CLIENT_ID = uuid()
+
+let reducer: React.Reducer<estado, accion> =
+  (state, action) => {
+    switch (action.type) {
+      case 'SET_NOTES':
+        return { ...state, notes: action.notes, loading: false }
+      case 'ADD_NOTE':
+        return { ...state, notes: [action.note, ...state.notes] }
+      case 'RESET_FORM':
+        return { ...state, form: initialState.form }
+      case 'SET_INPUT':
+        return { ...state, form: { ...state.form, [action.name]: action.value } }
+      case 'ERROR':
+        return { ...state, loading: false, error: true }
+      default:
+        return state
+    }
+  }
+
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    fetchNotes()
+    const subscription = API.graphql(graphqlOperation(onCreateNote))
+    // @ts-ignore:
+      .subscribe({
+        next: update_notes
+      })
+    return () => subscription.unsubscribe()
+  }, [])
+
+  function update_notes(x:any){
+    const note = x.value.data.onCreateNote
+    if (CLIENT_ID === note.clientId) return
+    dispatch({ type: 'ADD_NOTE', note })
+  }
+
+  async function fetchNotes() {
+    try {
+      //Recupera los datos
+      //let notesData: { data: { listnotes: items[] } }
+      const notesData = await API.graphql(graphqlOperation(listNotes))
+      // @ts-ignore:
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
+
+    } catch (err) {
+      console.log('error: ', err)
+      dispatch({ type: 'ERROR' })
+    }
+  }
+
+  function onChange(e:React.ChangeEvent<HTMLInputElement>) {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
+  }
+
+  async function createNote() {
+    const { form } = state
+    if (!form.name || !form.description) return alert('por favor, introduzca un nombre y descripción')
+    const note = { ...form, clientId: CLIENT_ID, completed: false }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+    try {
+      await API.graphql(graphqlOperation(CreateNote, { input: note }))
+      console.log('se ha creado la nota!')
+    } catch (err) {
+      console.log("error: ", err)
+    }
+  }
+
+  async function updateNote(note:nota) {
+    const index = state.notes.findIndex(n => n.id === note.id)
+    const notes = [...state.notes]
+    notes[index].completed = !note.completed
+    dispatch({ type: 'SET_NOTES', notes })
+    try {
+      await API.graphql(graphqlOperation(UpdateNote, { input: notes[index] }))
+      console.log('se ha actualizado la nota!')
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
+
+  async function deleteNote( id:string|undefined) {
+    const index = state.notes.findIndex(n => n.id === id)
+    const notes = [...state.notes.slice(0, index), ...state.notes.slice(index + 1)];
+    dispatch({ type: 'SET_NOTES', notes })
+    try {
+      await API.graphql(graphqlOperation(DeleteNote, { input: { id } }))
+      console.log('se borro la nota!')
+    } catch (err) {
+      console.log({ err })
+    }
+  }
+
+  function renderItem(item:nota) {
+    return (
+      <List.Item
+        style={styles.item}
+        actions={[
+          <p style={styles.p} onClick={() => deleteNote(item.id)}>Delete</p>,
+          <p style={styles.p} onClick={() => updateNote(item)}>
+            {item.completed ? 'completed' : 'mark completed'}
+          </p>
+        ]}
+      >
+        <List.Item.Meta
+          title={item.name}
+          description={item.description}
+        />
+      </List.Item>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <Input
+        onChange={onChange}
+        value={state.form?.name}
+        placeholder="Note Name"
+        name='name'
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form?.description}
+        placeholder="Note description"
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >Create Note</Button>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   );
 }
